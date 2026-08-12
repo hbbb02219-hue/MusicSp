e44e51bd7691599c98f89a2915eed4bdf2860e6c
Hindu community <hbbb02219@gmail.com>
2026-08-12 07:53:30 +0530

    Update Youtube.py

--- MusicSp/platforms/Youtube.py
+++ MusicSp/platforms/Youtube.py
@@ -8,9 +8,9 @@ from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
 import aiohttp
 
-API_URL = os.environ.get("MusicSp_API_URL", None)
+API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
 
-API_KEY = os.environ.get("MusicSp_API_KEY", None) ## Get This API KEY FROM OWNER: @SpYtAPIBot 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "YOUR_API_KEY") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
