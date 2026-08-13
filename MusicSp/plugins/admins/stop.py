a2c3ad3d71a23145d5a41ac4f1593eaa57059c26
Hindu community <hbbb02219@gmail.com>
2026-08-13 20:03:34 +0530

    Update stop.py

--- MusicSp/plugins/admins/stop.py
+++ MusicSp/plugins/admins/stop.py
@@ -6,6 +6,7 @@ from MusicSp.core.call import DevSp
 from MusicSp.utils.database import set_loop
 from MusicSp.utils.decorators import AdminRightsCheck
 from MusicSp.utils.inline import close_markup
+from MusicSp.utils.logger import activity_logs
 from config import BANNED_USERS
 
 
@@ -16,6 +17,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
+    await activity_logs(message, "Stop")
     await DevSp.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
